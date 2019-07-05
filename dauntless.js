b3321f4b111a762f5744d11469dc43fca451f849
Marcus Sakoda <msakoda@MACMSAKMBP15A.local>
2019-07-05 14:13:27 -0700

    updated login logic

--- dauntless.js
+++ dauntless.js
@@ -4,55 +4,105 @@ console.log(`running dauntless.js`);
 let username = document.querySelector(`#username`);
 let logoutLink = document.querySelector(`#logout`);
 let loginForm = document.querySelector(`#loginForm`);
+var users;
+
+/* Variabltes */
+const orbTypes = ['fire','frost','nature','shock','neutral','dire','heroic','heroic+'];
 
 initialize = function() {
 
-  // Check for logged in user
-  if (localStorage.username) {
-    login();
-  } else {
-    loginCard.classList.remove('d-none');
+  if (localStorage.users == undefined) {
+    localStorage.users = JSON.stringify([]);
   }
+
+  login();
 
 }
 
 login = function() {
-  username.innerText = `${localStorage.username}`;
+
+  let loggedInUser = JSON.parse(localStorage.users).find(u => u.isLoggedIn);
+
+  if (!loggedInUser) {
+    logoutLink.classList.add('d-none');
+    loginCard.classList.remove('d-none');
+    return;
+  }
+
+  username.innerText = `${loggedInUser.username}`;
   logoutLink.classList.remove('d-none');
   document.querySelector(`input[name='username']`).value = '';
   loginCard.classList.add('d-none');
-  onMessage(`Successfully logged in as ${localStorage.username}`,'success');
+  onMessage(`Successfully logged in as ${loggedInUser.username}`,'success');
 }
 
 logout = function() {
   username.innerText = '';
   let un = localStorage.username;
-  delete localStorage.username;
+  let loggedInUser = getLoggedInUser();
+  loggedInUser.isLoggedIn = false;
+  console.log(`loggedInUser:`,loggedInUser);
+  updateUsers(loggedInUser);
   logoutLink.classList.add('d-none');
   loginCard.classList.remove('d-none');
   onMessage(`Successfully logged out ${un}`,'success');
 }
 
+getLoggedInUser = function() {
+  return JSON.parse(localStorage.users).find(u => u.isLoggedIn);
+}
+
+updateUsers = function(user) {
+  let users = JSON.parse(localStorage.users);
+  let userIdx = users.findIndex(u => u.username == user.username);
+  users[userIdx] = user;
+  localStorage.users = JSON.stringify(users);
+}
+
 
 onLogin = function(){
-  console.log(`clicked login`);
+
+  // get username input
   let usernameInput = document.querySelector(`input[name='username']`);
+
+  // get value
   let val = usernameInput.value;
+
+  // check if empty
   if (val.trim() == "") {
     console.log(`username is empty`);
+    return;
   }
+
+  // parse users from localStorage
+  let users = JSON.parse(localStorage.users);
+  console.log(`users:`,users);
+
+  // look for existing user
+  let user = users.find(u => u.username.toLowerCase() == val.toLowerCase());
+
+  console.log(`user:`,user);
+
+  // create if it doesn't exist
+  if (!user) {
+    user = createUser(val);
+    users.forEach(u => u.isLoggedIn = false);
+    users.push(user);
+  }
+
+  console.log("user:",user)
+
   // log the user in
-  else {
-    localStorage.username = val;
-    login();
-  }
-  return false;
+  user.isLoggedIn = true;
+
+  // save the users in localStorage
+  localStorage.users = JSON.stringify(users);
+
+  login();
 }
 
 onLogout = function() {
-  if (localStorage.username) {
     logout();
-  }
 }
 
 onMessage = function(message,type) {
@@ -82,6 +132,23 @@ loginForm.addEventListener('submit',function(e) {
   e.preventDefault();
   onLogin();
 });
+
+
+// create a new user
+createUser = function(username) {
+  let user = {
+    username: username,
+    orbs: orbTypes.map(ot => {
+      return {
+        name:ot,
+        amt: 0
+      }
+    }),
+    isLoggedIn: false
+  };
+  console.log(`created user ${username}:`,user);
+  return user;
+}
 
 
 initialize();
